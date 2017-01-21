97c928f2352a910effffab257449c2e2fd27a689
Mike <michael.nyberghjerto@outlook.com>
2017-01-21 17:51:41 +0100

    added comment for test purpose (cherry picked from commit 51a9180)

--- app/src/main/java/com/example/morgan/WorldOfMikMog/Main.java
+++ app/src/main/java/com/example/morgan/WorldOfMikMog/Main.java
@@ -8,7 +8,6 @@ public class Main extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);// herregud
-        //hur funkar detta?
+        setContentView(R.layout.activity_main);
     }
 }
