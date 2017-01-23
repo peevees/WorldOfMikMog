fbfdc71fd7c6f9c916b378e4dfe788b65eb33247
Mike <michael.nyberghjerto@outlook.com>
2017-01-23 12:07:19 +0100

    added methods for buttons

--- app/src/main/java/com/example/morgan/WorldOfMikMog/Main.java
+++ app/src/main/java/com/example/morgan/WorldOfMikMog/Main.java
@@ -2,6 +2,9 @@ package com.example.morgan.WorldOfMikMog;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
 public class Main extends AppCompatActivity {
 
@@ -10,5 +13,31 @@ public class Main extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //test to see if it updates the repository
+
     }
+
+    // /better to assign method to xml layout
+    public void leftClick(View view){
+        //something happens
+        Log.d("myTag", "it went left");
+        TextView textView = (TextView)findViewById(R.id.textView1);
+        textView.setText("Went left");
+
+    }
+    public void rightClick(){
+        Log.d("myTag", "it went right");
+        TextView textView = (TextView)findViewById(R.id.textView1);
+        textView.setText("Went right");
+    }
+    public void upClick(){
+        Log.d("myTag", "it went up");
+        TextView textView = (TextView)findViewById(R.id.textView1);
+        textView.setText("Went up");
+    }
+    public void downClick(){
+        Log.d("myTag", "it went down");
+        TextView textView = (TextView)findViewById(R.id.textView1);
+        textView.setText("Went down");
+    }
+
 }
