ad0a9137bb0680820606724243d82caa144cdd62
Moggelicious <Moggelicious@hotmail.com>
2017-02-03 16:30:30 +0100

    0.2

--- app/src/main/java/com/example/morgan/WorldOfMikMog/Main.java
+++ app/src/main/java/com/example/morgan/WorldOfMikMog/Main.java
@@ -1,11 +1,14 @@
 package com.example.morgan.WorldOfMikMog;
 
+import android.content.Context;
 import android.graphics.Point;
 import android.media.MediaPlayer;
+import android.support.v4.view.GestureDetectorCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Display;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
@@ -17,6 +20,7 @@ import android.widget.TextView;
 public class Main extends AppCompatActivity {
 
     private ImageView player;
+    private GestureDetectorCompat detector;
     private TextView direction;
 
     //Sounds and music
@@ -37,16 +41,22 @@ public class Main extends AppCompatActivity {
     //speed
     private int playerSpeed;
 
-
+private Context context;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        detector = new GestureDetectorCompat(context, onSwipeListener);
+
+
+
         //test to see if it updates the repository
         direction = (TextView) findViewById(R.id.textView1);
         player = (ImageView) findViewById(R.id.player);
         playerHeight = player.getHeight();
         playerWidth = player.getWidth();
+
 
         //ljud
         sound = new SoundPlayer(this);
@@ -70,11 +80,11 @@ public class Main extends AppCompatActivity {
 
     }
 
-    public void leftClick(View view){
+    public void leftClick(View view) {
         //something happens
         Log.d("left", "it went left");
         direction.setText("Went left");
-        ((ViewGroup.MarginLayoutParams)player.getLayoutParams()).leftMargin-=20;
+        ((ViewGroup.MarginLayoutParams) player.getLayoutParams()).leftMargin -= 20;
         player.requestLayout();
 
 
@@ -82,34 +92,37 @@ public class Main extends AppCompatActivity {
         Log.d("position", String.valueOf(position[0]));
         Log.d("position", String.valueOf(position[1]));
     }
-    public void rightClick(View view){
+
+    public void rightClick(View view) {
         Log.d("right", "It went right");
         direction.setText("Went right");
-        ((ViewGroup.MarginLayoutParams)player.getLayoutParams()).leftMargin+=20;
+        ((ViewGroup.MarginLayoutParams) player.getLayoutParams()).leftMargin += 20;
         player.requestLayout();
 
         player.getLocationOnScreen(position);
         Log.d("position", String.valueOf(position[0]));
         Log.d("position", String.valueOf(position[1]));
     }
-    public void upClick(View view){
+
+    public void upClick(View view) {
         Log.d("TEST", "Innan ljud");
         sound.playWalkSound();
         Log.d("TEST", "efter ljud");
         Log.d("up", "it went up");
         direction.setText("Went up");
-        ((ViewGroup.MarginLayoutParams)player.getLayoutParams()).topMargin-=20;
+        ((ViewGroup.MarginLayoutParams) player.getLayoutParams()).topMargin -= 20;
         player.requestLayout();
 
         player.getLocationOnScreen(position);
         Log.d("position", String.valueOf(position[0]));
         Log.d("position", String.valueOf(position[1]));
     }
-    public void downClick(View view){
+
+    public void downClick(View view) {
         sound.playWalkSound();
         Log.d("down", "it went down");
         direction.setText("Went down");
-        ((ViewGroup.MarginLayoutParams)player.getLayoutParams()).topMargin+=20;
+        ((ViewGroup.MarginLayoutParams) player.getLayoutParams()).topMargin += 20;
         player.requestLayout();
 
         player.getLocationOnScreen(position);
@@ -118,8 +131,30 @@ public class Main extends AppCompatActivity {
     }
 
     //change position
-    public void changePos(){
+    public void changePos() {
 
     }
+    public boolean onTouch(View view, MotionEvent motionEvent) {
+        return detector.onTouchEvent(motionEvent);
+    }
 
-}
+    OnSwipeListener onSwipeListener = new OnSwipeListener() {
+
+        @Override
+        public boolean onSwipe(Direction direction) {
+
+            // Possible implementation
+            if(direction == Direction.left|| direction == Direction.right) {
+                // Do something COOL like animation or whatever you want
+                // Refer to your view if needed using a global reference
+                return true;
+            }
+            else if(direction == Direction.up|| direction == Direction.down) {
+                // Do something COOL like animation or whatever you want
+                // Refer to your view if needed using a global reference
+                return true;
+            }
+
+            return super.onSwipe(direction);
+        }
+};}
