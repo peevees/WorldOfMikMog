74c93ce4e5f21b6cee626024f29920e7809c0964
Mike <michael.nyberghjerto@outlook.com>
2017-02-13 15:14:43 +0100

    commit

--- app/src/main/java/com/example/morgan/WorldOfMikMog/PlayerView.java
+++ app/src/main/java/com/example/morgan/WorldOfMikMog/PlayerView.java
@@ -13,6 +13,14 @@ public class PlayerView extends ImageView {
     private float touchStartY;
     private float touchEndX;
     private float touchEndY;
+    private float deltaY;
+    private float deltaX;
+
+    private ImageView player;
+    private float playerY;
+    private float playerX;
+    private float playerWidth;
+    private float playerHeight;
 
     public PlayerView(Context context){
         super(context);
@@ -25,16 +33,42 @@ public class PlayerView extends ImageView {
 
         int action = MotionEventCompat.getActionMasked(event);
 
+        player = (ImageView) findViewById(R.id.player);
+        playerY = player.getY();
+        playerX = player.getX();
+        playerWidth = player.getWidth();
+        playerHeight = player.getHeight();
+
         switch(action){
             case(MotionEvent.ACTION_DOWN):
                 touchStartX = event.getX();
                 touchStartY = event.getY();
-                Log.d("PRESSED_DOWN", "press down on: " + touchStartX + ", " + touchStartY);
+
+               // Log.d("PRESSED_DOWN", "press down on: " + touchStartX + ", " + touchStartY);
                 return true;
             case(MotionEvent.ACTION_UP):
                 touchEndX = event.getX();
                 touchEndY = event.getY();
-                Log.d("PRESSED_DOWN", "press down on: " + touchEndX + ", " + touchEndY);
+
+                //Log.d("PRESSED_UP", "press up on: " + touchEndX + ", " + touchEndY);
+                deltaY = touchStartY - touchEndY;
+                deltaX = touchStartX - touchEndX;
+                Log.d("SWIPED", "Swiped with delta: " + deltaX + ", " + deltaY);
+
+                if(deltaY > 50 && deltaX < 10){//up
+                    Log.d("APP", " inside the allowed swipe UP area");
+                    player.setY(playerY - playerHeight);
+                }else if(deltaY < -50 && deltaX < 10){//down
+                    Log.d("APP", " inside the allowed swipe DOWN area");
+                    player.setY(playerY + playerHeight);
+                }else if(deltaY < 10 && deltaX > 50){//left
+                    Log.d("APP", " inside the allowed swipe LEFT area");
+                    player.setX(playerX - playerWidth);
+                }else if(deltaY < 10 && deltaX < -50){//right
+                    Log.d("APP", " inside the allowed swipe RIGHT area");
+                    player.setX(playerX + playerWidth);
+                }
+
                 return true;
             default:
                 return super.onTouchEvent(event);
