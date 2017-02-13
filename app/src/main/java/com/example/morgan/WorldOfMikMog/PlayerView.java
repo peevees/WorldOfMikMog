364f31aa5078bb3fadd5bdacea283fd3329e9500
Mike <michael.nyberghjerto@outlook.com>
2017-02-13 13:20:21 +0100

    world height is now dependent on screen size and starting on collecting screen input it does however not work yet

--- app/src/main/java/com/example/morgan/WorldOfMikMog/PlayerView.java
+++ app/src/main/java/com/example/morgan/WorldOfMikMog/PlayerView.java
@@ -9,6 +9,7 @@ import android.widget.ImageView;
 
 public class PlayerView extends ImageView {
 
+    //movement
     private float touchStartX;
     private float touchStartY;
     private float touchEndX;
@@ -16,18 +17,28 @@ public class PlayerView extends ImageView {
     private float deltaY;
     private float deltaX;
 
+    //screen
+    private int screenWidth;
+    private int screenHeight;
+
+    //player
     private ImageView player;
     private float playerY;
     private float playerX;
     private float playerWidth;
     private float playerHeight;
+    private int playerPosX = 0;
+    private int playerPosY = 0;
 
-    public PlayerView(Context context){
+    public PlayerView(Context context, int screenW, int screenH){//constructor
         super(context);
+        this.screenWidth = screenW;
+        this.screenHeight = screenH;
     }
     public PlayerView(Context context, AttributeSet attrs){
         super(context, attrs);
     }
+
     @Override
     public boolean onTouchEvent(MotionEvent event){
 
@@ -43,36 +54,70 @@ public class PlayerView extends ImageView {
             case(MotionEvent.ACTION_DOWN):
                 touchStartX = event.getX();
                 touchStartY = event.getY();
-
-               // Log.d("PRESSED_DOWN", "press down on: " + touchStartX + ", " + touchStartY);
                 return true;
             case(MotionEvent.ACTION_UP):
                 touchEndX = event.getX();
                 touchEndY = event.getY();
-
-                //Log.d("PRESSED_UP", "press up on: " + touchEndX + ", " + touchEndY);
                 deltaY = touchStartY - touchEndY;
                 deltaX = touchStartX - touchEndX;
-                Log.d("SWIPED", "Swiped with delta: " + deltaX + ", " + deltaY);
+                if (deltaY > 50 && deltaX < 10) {//up
+                    if(((Main)getContext()).moveAllowed(playerPosX, playerPosY-1)){
+                        playerPosY--;
 
-                if(deltaY > 50 && deltaX < 10){//up
-                    Log.d("APP", " inside the allowed swipe UP area");
-                    player.setY(playerY - playerHeight);
-                }else if(deltaY < -50 && deltaX < 10){//down
-                    Log.d("APP", " inside the allowed swipe DOWN area");
-                    player.setY(playerY + playerHeight);
-                }else if(deltaY < 10 && deltaX > 50){//left
-                    Log.d("APP", " inside the allowed swipe LEFT area");
-                    player.setX(playerX - playerWidth);
-                }else if(deltaY < 10 && deltaX < -50){//right
-                    Log.d("APP", " inside the allowed swipe RIGHT area");
-                    player.setX(playerX + playerWidth);
+                        player.setY(playerY - playerHeight);
+                        playerY = player.getY();
+                    }
+                    Log.d("POS_AFTER_MOVEMENT", "UP move playerY: " + playerY + " playerPosY: " + playerPosY);
+                    /*
+                    if(playerY - playerHeight >= 0) {
+                        player.setY(playerY - playerHeight);
+                        playerY = player.getY();
+                    }
+                    */
+                } else if (deltaY < -50 && deltaX < 10) {//down
+                    if(((Main)getContext()).moveAllowed(playerPosX, playerPosY+1)){
+                        playerPosY++;
+                        player.setY(playerY + playerHeight);
+                        playerY = player.getY();
+                    }
+                    Log.d("POS_AFTER_MOVEMENT", "DOWN move playerY: " + playerY + " playerPosY: " + playerPosY);
+                    /*
+                    if(playerY + playerHeight < screenHeight - playerHeight) {
+                        player.setY(playerY + playerHeight);
+                        playerY = player.getY();
+                    }
+                    */
+                } else if (deltaY < 10 && deltaX > 50) {//left
+                    if(((Main)getContext()).moveAllowed(playerPosX-1, playerPosY)){
+                        playerPosX--;
+                        player.setX(playerX - playerWidth);
+                        playerX = player.getX();
+
+                    }
+                    Log.d("POS_AFTER_MOVEMENT", "LEFT move playerX: " + playerX + " playerPosX: " + playerPosX);
+                    /*
+                    if(playerX - playerWidth <= 0) {
+                        player.setX(playerX - playerWidth);
+                        playerX = player.getX();
+                    }
+                    */
+                } else if (deltaY < 10 && deltaX < -50) {//right
+                    if(((Main)getContext()).moveAllowed(playerPosX+1, playerPosY)){
+                        playerPosX++;
+                        player.setX(playerX + playerWidth);
+                        playerX = player.getX();
+                    }
+                    Log.d("POS_AFTER_MOVEMENT", "RIGHT move playerX: " + playerX + " playerPosX: " + playerPosX);
+                    /*
+                    if(playerX + playerWidth < screenWidth - playerWidth) {
+                        player.setX(playerX + playerWidth);
+                        playerX = player.getX();
+                    }
+                    */
                 }
-
                 return true;
             default:
                 return super.onTouchEvent(event);
         }
-
     }
 }
