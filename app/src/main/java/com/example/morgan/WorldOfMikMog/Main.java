fbce9db00761a18b52538c113a462cfe5e7f62e6
Mike <michael.nyberghjerto@outlook.com>
2017-02-13 13:36:34 +0100

    commit

--- app/src/main/java/com/example/morgan/WorldOfMikMog/Main.java
+++ app/src/main/java/com/example/morgan/WorldOfMikMog/Main.java
@@ -56,7 +56,7 @@ public class Main extends AppCompatActivity implements View.OnTouchListener {
         //set immersive mode
         getWindow().getDecorView().setSystemUiVisibility(
                 View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
                         | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
                         | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
                         | View.SYSTEM_UI_FLAG_FULLSCREEN
