f9fa6eea718a1bcb270f81c4f788d354fae6ed57
Mike <michael.nyberghjerto@outlook.com>
2017-02-14 15:00:47 +0100

    world creation done, and cleaning out code that isnt used

--- app/src/main/java/com/example/morgan/WorldOfMikMog/PlayerView.java
+++ app/src/main/java/com/example/morgan/WorldOfMikMog/PlayerView.java
@@ -9,7 +9,6 @@ import android.widget.ImageView;
 
 public class PlayerView extends ImageView {
 
-
     //movement
     private float touchStartX;
     private float touchStartY;
@@ -29,18 +28,13 @@ public class PlayerView extends ImageView {
     private float playerWidth;
     private float playerHeight;
 
-
-    public PlayerView(Context context, int screenW, int screenH){
+    public PlayerView(Context context, int screenW, int screenH){//constructor
         super(context);
         this.screenWidth = screenW;
         this.screenHeight = screenH;
     }
     public PlayerView(Context context, AttributeSet attrs){
         super(context, attrs);
-    }
-
-    public void insideGameArea(){
-
     }
 
     @Override
@@ -58,69 +52,36 @@ public class PlayerView extends ImageView {
             case(MotionEvent.ACTION_DOWN):
                 touchStartX = event.getX();
                 touchStartY = event.getY();
-
-               // Log.d("PRESSED_DOWN", "press down on: " + touchStartX + ", " + touchStartY);
                 return true;
             case(MotionEvent.ACTION_UP):
                 touchEndX = event.getX();
                 touchEndY = event.getY();
-
-                //Log.d("PRESSED_UP", "press up on: " + touchEndX + ", " + touchEndY);
                 deltaY = touchStartY - touchEndY;
                 deltaX = touchStartX - touchEndX;
-                Log.d("SWIPED", "Swiped with delta: " + deltaX + ", " + deltaY);
-
                 if (deltaY > 50 && deltaX < 10) {//up
-                    Log.d("APP", " inside the allowed swipe UP area");
-
-                    Log.d("PLAYER_POS", "player was at: " + playerX + ", " + playerY);
-
                     if(playerY - playerHeight >= 0) {
                         player.setY(playerY - playerHeight);
                         playerY = player.getY();
-                        Log.d("PLAYER_POS", "player is now at: " + playerX + ", " + playerY);
                     }
-
                 } else if (deltaY < -50 && deltaX < 10) {//down
-                    Log.d("APP", " inside the allowed swipe DOWN area");
-
-                    Log.d("PLAYER_POS", "player was at: " + playerX + ", " + playerY);
-
                     if(playerY + playerHeight < screenHeight - playerHeight) {
                         player.setY(playerY + playerHeight);
                         playerY = player.getY();
-                        Log.d("PLAYER_POS", "player is now at: " + playerX + ", " + playerY);
                     }
-
                 } else if (deltaY < 10 && deltaX > 50) {//left
-                    Log.d("APP", " inside the allowed swipe LEFT area");
-
-                    Log.d("PLAYER_POS", "player was at: " + playerX + ", " + playerY);
-
                     if(playerX - playerWidth <= 0) {
                         player.setX(playerX - playerWidth);
                         playerX = player.getX();
-                        Log.d("PLAYER_POS", "player is now at: " + playerX + ", " + playerY);
                     }
-
                 } else if (deltaY < 10 && deltaX < -50) {//right
-                    Log.d("APP", " inside the allowed swipe RIGHT area");
-
-                    Log.d("TEST", "screen width: " + screenWidth);
-                    Log.d("TEST", "screenwidth after removal of player width: " + (screenWidth - playerWidth));
-                    Log.d("PLAYER_POS", "player was at: " + playerX + ", " + playerY);
                     if(playerX + playerWidth < screenWidth - playerWidth) {
                         player.setX(playerX + playerWidth);
                         playerX = player.getX();
-                        Log.d("PLAYER_POS", "player is now at: " + playerX + ", " + playerY);
                     }
                 }
-
                 return true;
-
             default:
                 return super.onTouchEvent(event);
         }
-
     }
 }
