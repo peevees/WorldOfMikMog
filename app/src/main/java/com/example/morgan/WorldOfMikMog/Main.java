2443c850661e9692f198698cbfd0764ac7c8e751
Mike <michael.nyberghjerto@outlook.com>
2017-02-16 11:39:50 +0100

    fixed navigation bar

--- app/src/main/java/com/example/morgan/WorldOfMikMog/Main.java
+++ app/src/main/java/com/example/morgan/WorldOfMikMog/Main.java
@@ -43,11 +43,16 @@ public class Main extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //set immersive mode
+        /*
         getWindow().getDecorView().setSystemUiVisibility(
                 View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                         | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
                         | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
                         | View.SYSTEM_UI_FLAG_FULLSCREEN);
+                        */
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         //music & sound
         sound = new SoundPlayer(this);
