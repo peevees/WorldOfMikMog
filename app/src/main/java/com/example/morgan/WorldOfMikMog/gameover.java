f89c73858136e64666733a70359505744ad4f9c8
Moggelicious <Moggelicious@hotmail.com>
2017-02-18 16:03:19 +0100

    making the endScreen layout

--- app/src/main/java/com/example/morgan/WorldOfMikMog/gameover.java
+++ app/src/main/java/com/example/morgan/WorldOfMikMog/gameover.java
@@ -12,7 +12,7 @@ import android.view.WindowManager;
  * Created by Morgan on 2017-02-16.
  */
 
-public class GameOver extends AppCompatActivity{
+public class gameOver extends AppCompatActivity{
 
     protected MediaPlayer sound;
 
